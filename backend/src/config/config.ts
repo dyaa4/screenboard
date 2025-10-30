e7ce4eeaed7d929603410d610b0fe3b91d11b687
Dyaa4 <dyaa.kassoma@gmail.com>
2025-10-30 21:04:58 +0100

    Update server configuration to use '0.0.0.0' for host and standardize port variable

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,9 +1,8 @@
-
 export const config = {
-    port: parseInt(process.env.BACKEND_PORT || '3001', 10),
-    host: process.env.HOST || 'localhost',
-    auth0: {
-      domain: process.env.AUTH0_DOMAIN,
-      audience: process.env.AUTH0_AUDIENCE
-    },
-  };
+  port: parseInt(process.env.PORT || '3001', 10),
+  host: '0.0.0.0', // wichtig: damit der Server von außen erreichbar ist
+  auth0: {
+    domain: process.env.AUTH0_DOMAIN,
+    audience: process.env.AUTH0_AUDIENCE
+  },
+};
